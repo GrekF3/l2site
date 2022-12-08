53e3870ac1cf2ea1826246574d471658e6c696cd
Nikita <v.beats@inbox.ru>
2022-12-08 13:41:15 +0300

    Фиксы,фиксы,фиксы

--- l2/l2servers/views.py
+++ l2/l2servers/views.py
@@ -10,12 +10,12 @@ from .models import GameServer, Ads
 def index(request):
     lineage2_servers = GameServer.objects.all().filter(online_game='lineage-2').filter(moderate=True)
     rust_servers = GameServer.objects.all().filter(online_game='Rust').filter(moderate=True)
-    wow_servers = GameServer.objects.all().filter(online_game='Minecraft').filter(moderate=True)
-    csgo_servers = GameServer.objects.all().filter(online_game='Word_of_Warcraft').filter(moderate=True)
-    arma3_servers = GameServer.objects.all().filter(online_game='csgo').filter(moderate=True)
-    dayz_servers = GameServer.objects.all().filter(online_game='arma3').filter(moderate=True)
-    others_servers = GameServer.objects.all().filter(online_game='DayZ').filter(moderate=True)
-    minecraft_servers = GameServer.objects.all().filter(online_game='Others').filter(moderate=True)
+    wow_servers = GameServer.objects.all().filter(online_game='Word_of_Warcraft').filter(moderate=True)
+    csgo_servers = GameServer.objects.all().filter(online_game='csgo').filter(moderate=True)
+    arma3_servers = GameServer.objects.all().filter(online_game='arma3').filter(moderate=True)
+    dayz_servers = GameServer.objects.all().filter(online_game='DayZ').filter(moderate=True)
+    others_servers = GameServer.objects.all().filter(online_game='others').filter(moderate=True)
+    minecraft_servers = GameServer.objects.all().filter(online_game='Minecraft').filter(moderate=True)
 
     all_server = GameServer.objects.all().filter(moderate=True).count()
     moderate = GameServer.objects.all().filter(moderate=False).count()
@@ -44,14 +44,15 @@ class ServersListView(ListView):
 
     def get_queryset(self):
         qs = self.model.objects.all()
+        print(qs)
         if self.kwargs.get('game_slug'):
-            qs = self.model.objects.filter(online_game=self.kwargs['game_slug']).filter(ads=None)
-            print(self.kwargs['game_slug'])
+            qs = self.model.objects.filter(online_game__iexact=self.kwargs['game_slug']).filter(ads=None)
+            print(qs)
         return qs
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data()
-        context['ads'] = Ads.objects.all().filter(server__online_game=self.kwargs['game_slug'])
+        context['ads'] = Ads.objects.all().filter(server__online_game__iexact=self.kwargs['game_slug'])
         return context
 
 @login_required
